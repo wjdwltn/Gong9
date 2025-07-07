fcf7615aaeff3e153febcfed323b6ce9437dc9cc
wjdwltn <jlsowltn2000@naver.com>
2025-07-07 14:06:17 +0900

    first commit

---
+++ src/main/java/com/gg/gong9/Gong9Application.java
@@ -0,0 +1,13 @@
+package com.gg.gong9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gong9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gong9Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gg/gong9/Gong9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gg.gong9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gong9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
