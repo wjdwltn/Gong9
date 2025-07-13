ae4fa2be56089d4bcfee4e86578fc7d022041185
wjdwltn <jlsowltn2000@naver.com>
2025-07-13 23:17:05 +0900

    feat : User Repository 코드 작성

---
+++ src/main/java/com/gg/gong9/user/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.gg.gong9.user.repository;
+
+import com.gg.gong9.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    //이메일로 사용자 조회
+    Optional<User> findByEmail(String email);
+
+    //회원가입 시 이메일 중복 확인
+    boolean existsByEmail(String email);
+}
