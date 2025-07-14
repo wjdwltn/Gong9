165bb42d37a9763a49e483897b42cb4ac374da4a
wjdwltn <jlsowltn2000@naver.com>
2025-07-14 19:45:09 +0900

    feat : UserException 구현

--- src/main/java/com/gg/gong9/global/exception/exceptions/UserException.java
+++ src/main/java/com/gg/gong9/global/exception/exceptions/UserException.java
@@ -5,6 +5,6 @@ import com.gg.gong9.global.exception.ExceptionMessage;
 
 public class UserException extends BaseException {
     public UserException(ExceptionMessage message) {
-        super(message.getText());
+        super(message.getText(), message.getStatus());
     }
 }
