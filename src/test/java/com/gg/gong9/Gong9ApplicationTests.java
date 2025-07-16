ad53e1e4b0fe6623b1194aee8cefe736dea49239
wjdwltn <jlsowltn2000@naver.com>
2025-07-17 00:19:49 +0900

    Fix : CI-gradlew-permission

--- src/test/java/com/gg/gong9/Gong9ApplicationTests.java
+++ src/test/java/com/gg/gong9/Gong9ApplicationTests.java
@@ -3,7 +3,7 @@ package com.gg.gong9;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Gong9Application.class)
 class Gong9ApplicationTests {
 
     @Test
