620fb9dd8b293dc3d20c0c57a668a62d3944deb5
wjdwltn <jlsowltn2000@naver.com>
2025-07-21 15:13:55 +0900

    Feat: CORS 설정

--- src/main/java/com/gg/gong9/global/security/SecurityConfig.java
+++ src/main/java/com/gg/gong9/global/security/SecurityConfig.java
@@ -14,8 +14,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
+
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -35,6 +42,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 //csrf disable
                 .csrf(AbstractHttpConfigurer::disable)
                 //Form login disable
@@ -65,4 +73,17 @@ public class SecurityConfig {
         return authenticationManagerBuilder.build();
     }
 
+    /** cors 설정 bean */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Collections.singletonList("*")); // "*" 대신 패턴 사용
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
