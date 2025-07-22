1ebd12d63fb76600dbe5e103ef472ab63819174f
김지은 <wldms5119@naver.com>
2025-07-22 19:38:02 +0900

    feat: 공구 등록 시 상품 등록자 검증 & 예외 처리 추가, 중복 코드 제거

--- src/main/java/com/gg/gong9/global/exception/exceptions/groupbuy/GroupBuyExceptionMessage.java
+++ src/main/java/com/gg/gong9/global/exception/exceptions/groupbuy/GroupBuyExceptionMessage.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum GroupBuyExceptionMessage {
-    NO_PERMISSION("해당 상품에 대한 권한이 없습니다.", HttpStatus.FORBIDDEN),
+    NO_PERMISSION_GROUPBUY("해당 공구에 대한 권한이 없습니다.", HttpStatus.FORBIDDEN),
     NOT_FOUND_GROUPBUY("해당 공동구매가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
     INVALID_OPERATION("결제된 수량보다 적은 총 수량으로 수정할 수 없습니다.", HttpStatus.BAD_REQUEST),
     ALREADY_ENDED("이미 종료된 공동구매는 취소할 수 없습니다.", HttpStatus.BAD_REQUEST),
