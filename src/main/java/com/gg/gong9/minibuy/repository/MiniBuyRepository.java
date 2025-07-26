8e26f58c4fc0ed7bd6135b79453af2455fc840da
김지은 <wldms5119@naver.com>
2025-07-27 00:53:48 +0900

    feat: 기본 MiniBuyRepository 작성

---
+++ src/main/java/com/gg/gong9/minibuy/repository/MiniBuyRepository.java
@@ -0,0 +1,11 @@
+package com.gg.gong9.minibuy.repository;
+
+
+import com.gg.gong9.minibuy.entity.MiniBuy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface MiniBuyRepository extends JpaRepository<MiniBuy,Long> {
+
+}
