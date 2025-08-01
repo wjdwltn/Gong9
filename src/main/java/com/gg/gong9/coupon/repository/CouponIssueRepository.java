8c63a26c7932a45d448f7c5365d106243a510644
김지은 <wldms5119@naver.com>
2025-08-02 03:22:40 +0900

    feature: 기본 CouponIssueRepository 작성

---
+++ src/main/java/com/gg/gong9/coupon/repository/CouponIssueRepository.java
@@ -0,0 +1,11 @@
+package com.gg.gong9.coupon.repository;
+
+import com.gg.gong9.coupon.entity.CouponIssue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface CouponIssueRepository extends JpaRepository<CouponIssue, Long> {
+
+
+}
