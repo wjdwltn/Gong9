e8a8e10d4f703f4760d0e6b65b43bdff1e64bf81
김지은 <wldms5119@naver.com>
2025-08-19 02:40:33 +0900

    feat: 스케줄러 활성화 - @EnableScheduling 추가

--- src/main/java/com/gg/gong9/Gong9Application.java
+++ src/main/java/com/gg/gong9/Gong9Application.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableJpaAuditing
 @EnableFeignClients(basePackages = "com.gg.gong9.auth.kakao")
 public class Gong9Application {
